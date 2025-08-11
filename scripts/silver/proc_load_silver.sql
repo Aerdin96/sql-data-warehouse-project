4d1778d5412c948ca52b7fbea95ecf68e672036c
Aerdin96 <109162391+Aerdin96@users.noreply.github.com>
2025-08-11 09:47:25 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -46,10 +46,10 @@ BEGIN
 			cst_create_date
 		)
 		SELECT
-		cst_id,
-		cst_key,
-		TRIM(cst_firstname) AS cst_firstname,
-		TRIM(cst_lastname) AS cst_lastname,
+			cst_id,
+			cst_key,
+			TRIM(cst_firstname) AS cst_firstname,
+			TRIM(cst_lastname) AS cst_lastname,
 		CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
 			 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
 			 ELSE 'Unknown'
@@ -65,7 +65,8 @@ BEGIN
 			ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
 			FROM bronze.crm_cust_info
 			WHERE cst_id IS NOT NULL
-		)t WHERE flag_last = 1
+		)t 
+		WHERE flag_last = 1
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '--------------';
@@ -121,9 +122,9 @@ BEGIN
 			sls_price
 		)
 		SELECT
-		sls_ord_num,
-		sls_prd_key,
-		sls_cust_id,
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
 		CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
 			 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
 		END AS sls_order_dt,
@@ -208,10 +209,10 @@ BEGIN
 			maintenance
 		)
 		SELECT
-		id,
-		cat,
-		subcat,
-		maintenance
+			id,
+			cat,
+			subcat,
+			maintenance
 		FROM bronze.erp_px_cat_g1v2
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
