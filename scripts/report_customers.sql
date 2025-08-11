302d1fbcd80274b742a96a7c8b2dfec8e9f6d00d
Aerdin96 <109162391+Aerdin96@users.noreply.github.com>
2025-08-11 10:23:44 +0200

    Update report_customers.sql

--- scripts/report_customers.sql
+++ scripts/report_customers.sql
@@ -27,15 +27,15 @@ WITH base_query AS (
 1) Base Query: Retrieves core columns from tables
 --------------------------------------------------------------------------------*/
 SELECT
-f.order_number,
-f.product_key,
-f.order_date,
-f.sales_amount,
-f.quantity,
-c.customer_key,
-c.customer_number,
-CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
-DATEDIFF(year, c.birthdate, GETDATE()) AS age
+	f.order_number,
+	f.product_key,
+	f.order_date,
+	f.sales_amount,
+	f.quantity,
+	c.customer_key,
+	c.customer_number,
+	CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+	DATEDIFF(year, c.birthdate, GETDATE()) AS age
 FROM gold.fact_sales AS f
 LEFT JOIN gold.dim_customers AS c
 ON c.customer_key = f.customer_key
@@ -46,16 +46,16 @@ customer_aggregation AS (
 2) Customer Aggregations: Summarizes key metrics at the customer level
 --------------------------------------------------------------------------------*/
 SELECT
-customer_key,
-customer_number,
-customer_name,
-age,
-COUNT(DISTINCT order_number) AS total_orders,
-SUM(sales_amount) AS total_sales,
-SUM(quantity) AS total_quantity,
-COUNT(DISTINCT product_key) AS total_products,
-MAX(order_date) AS last_order_date,
-DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan
+	customer_key,
+	customer_number,
+	customer_name,
+	age,
+	COUNT(DISTINCT order_number) AS total_orders,
+	SUM(sales_amount) AS total_sales,
+	SUM(quantity) AS total_quantity,
+	COUNT(DISTINCT product_key) AS total_products,
+	MAX(order_date) AS last_order_date,
+	DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan
 FROM base_query
 GROUP BY
 	customer_key,
@@ -65,10 +65,10 @@ GROUP BY
 )
 
 SELECT
-customer_key,
-customer_number,
-customer_name,
-age,
+	customer_key,
+	customer_number,
+	customer_name,
+	age,
 CASE 
 	 WHEN age < 20 THEN 'Under 20'
 	 WHEN age BETWEEN 20 and 29 THEN '20-29'
@@ -81,13 +81,13 @@ CASE
 	 WHEN lifespan >= 12 AND total_sales < 5000 THEN 'Regular'
 	 ELSE 'New'
 END AS customer_segment,
-last_order_date,
-DATEDIFF(month, last_order_date, GETDATE()) AS recency,
-total_orders,
-total_sales,
-total_quantity,
-total_products,
-lifespan,
+	last_order_date,
+	DATEDIFF(month, last_order_date, GETDATE()) AS recency,
+	total_orders,
+	total_sales,
+	total_quantity,
+	total_products,
+	lifespan,
 -- Compute average order value (AVO)
 CASE 
 	 WHEN total_orders = 0 THEN 0
